06dff4894d06f4936180042e4b3f0ece684f3d44
Elizaveta-Makeeva <makeevaea18@gmail.com>
2025-12-08 21:27:40 +0300

    Updates

--- src/store.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit"
-import periodsReducer from "./slices/periodsSlice"
-
-export default configureStore({
-    reducer: combineReducers({
-        ourPeriods: periodsReducer
-    })
-})
